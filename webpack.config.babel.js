b3e538e53f577f67782bf907447af3f8bc22274d
Gar <gar@lonelydatu.com>
2017-02-25 22:28:35 -0500

    feat(sdf): sdfsdf

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -7,7 +7,7 @@ export default {
   output: {
     path: join(__dirname, 'dist'),
     libraryTarget: 'umd',
-    library: 'Freckles'
+    library: 'Handwrite'
   },
   devtool: 'source-map',
   module: {
