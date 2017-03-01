19123bdcafac02081d8a9764fd3634ff0ce7b74c
Gar <gar@lonelydatu.com>
2017-02-28 23:30:49 -0500

    feat(changed entry file): sldkf lsdkfj sd

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,7 +3,7 @@ import {join} from 'path'
 const include = join(__dirname, 'src')
 
 export default {
-  entry: './src/HandwriteES5__ENTRY.js',
+  entry: './src/Handwrite.js',
   output: {
     path: join(__dirname, 'dist'),
     libraryTarget: 'umd',
